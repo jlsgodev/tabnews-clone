e80d45be61af961ebff5768fade4280e2646cf2b
Jhon Gonçalves <jlsgo.dev@outlook.com>
2024-07-22 19:07:08 +0000

    move .env file to .env.development

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -17,3 +17,6 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
+
+
+   
