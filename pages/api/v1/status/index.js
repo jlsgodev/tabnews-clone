a3b156af57bfde62df488a0f6e8457150257e0c8
Jhon Gonçalves <jlsgo.dev@outlook.com>
2024-07-22 00:30:32 +0000

    adiciona database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,10 @@
-function status(req, res) {
+import database from "../../../database.js";
+
+async function status(req, res) {
+  const result = await database.query('SELECT 1 + 1 as sum;' )
+  console.log(result.rows;
   res.status(200).json({ chave: "OK!!!" });
 }
 export default status;
+
+
