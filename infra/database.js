976d8e96b37698437257f86e5444d48a5e586391
Jhon Gonçalves <jlsgo.dev@outlook.com>
2024-07-22 17:50:54 +0000

    add database.js and .env

--- infra/database.js
+++ infra/database.js
@@ -1,19 +1,46 @@
-import { Client} from 'pg';
+import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    database: 'postgres',
-    password: "LOCAL_PASSWORD",
-  });
-  await client.conect();
-  const result =await client.query(queryObject);
-  await client.end();
-  return result;
+  let client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
-export default {
-  query: query,
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
+const database = {
+  query,
+  getNewClient,
 };
+
+export default database;
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,8 @@
-import database from "../../../database.js";
+import database from "../../../../infra/database.js"; // Importando a conexão com o banco de dados
 
 async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1 as sum;' )
-  console.log(result.rows;
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   res.status(200).json({ chave: "OK!!!" });
 }
 export default status;
-
-
